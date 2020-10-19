cd5d2f6e5266aadb60b47e6f96fc0869381e52bb
Roman Konashenkov <rom.konashenkov@yandex.ru>
2020-10-19 17:32:44 +0300

    some fixes

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,4 @@
 module.exports = {
-    configureWebpack: {
-        module: {
-            rules: [
-                {
-                    test: /\.scss$/,
-                    use: [
-                        'vue-style-loader',
-                        'css-loader',
-                        'sass-loader'
-                    ]
-                }
-            ]
-        }
-    },
     publicPath: process.env.NODE_ENV === 'production'
     ? '/vue-vendor-machine/'
     : '/'
